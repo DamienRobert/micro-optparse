c1f69031237d6224c909bb0b45c5ceff3df2af51
Damien Robert <damien.olivier.robert+git@gmail.com>
2019-04-25 22:19:22 +0200

    Correct test

--- test/test_nano-optparse.rb
+++ test/test_nano-optparse.rb
@@ -4,7 +4,7 @@ require 'nano-optparse'
 class TestNanoOptparse < Minitest::Test
 
   def test_version
-    version = NanoOptparse.const_get('VERSION')
+    version = NanoParser.const_get('VERSION')
 
     assert(!version.empty?, 'should have a VERSION constant')
   end
